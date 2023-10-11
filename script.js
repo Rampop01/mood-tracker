7573a8583f98032f9afe94baa909047fe206f952
unknown <popoolarahmat96@gmail.com>
2023-10-12 01:14:56 +0200

    first commit

--- script.js
+++ script.js
@@ -37,3 +37,82 @@ const myChart = new Chart(ctx, {
         }
     }
 });
+
+
+
+// mooodtrack and mood note js---------------
+const dailyDate = document.querySelector(".everyDayDate");
+const userNote = document.forms[".userNote"];
+const noteInput = document.querySelector(".todayNoteInput");
+const inputSubmit = document.querySelector(".inputSubmit");
+const dailyMood = document.querySelector(".dailyMood");
+const dailyMoodAppend = document.querySelector(".dailyMoodAppend");
+
+// daily date--------------
+const now = new Date();
+const day = now.getDate();
+const month = now.toLocaleString("default", { month: "long" });
+const year = now.getFullYear();
+
+dailyDate.innerHTML = `${day}th ${month} ${year}`;
+
+// quote of the day---------------
+const quoteOpen = function () {
+  // overlay.classList.remove("hidden");
+  modal.classList.remove("hidden");
+};
+
+const quoteClose = function () {
+  overlay.classList.add("hidden");
+  modal.classList.add("hidden");
+};
+btnCloseModal.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+
+// emoji javascript---------------
+
+function excitedButton() {
+  document.getElementById("excitedCount").innerHTML++;
+}
+
+function veryHappyButton() {
+  document.getElementById("anxiousCount").innerHTML++;
+}
+
+function happyButton() {
+  document.getElementById("happyCount").innerHTML++;
+}
+
+function chillButton() {
+  document.getElementById("chillCount").innerHTML++;
+}
+function neutralButton() {
+  document.getElementById("neutralCount").innerHTML++;
+}
+function sadButton() {
+  document.getElementById("sadCount").innerHTML++;
+}
+function anxiousButton() {
+  document.getElementById("anxiousCount").innerHTML++;
+}
+function angryButton() {
+  document.getElementById("angryCount").innerHTML++;
+}
+function stressedButton() {
+  document.getElementById("stressedCount").innerHTML++;
+}
+
+// quote of the day---------------
+
+// user safe place for mood---------
+function addItem(e) {
+  e.preventDefault();
+  let userInput = document.getElementById("mood").value;
+  document.querySelector(".todayNoteInput").innerText += userInput;
+}
